9ba3c2f67f46c42943ea867fa0c47a8b247ddb1f
finnapx <63605614+finnapx@users.noreply.github.com>
2020-09-26 23:04:22 -0300

    Update pou.py

--- pou.py
+++ pou.py
@@ -1,19 +1,4 @@
-import numpy as np
-
-# [cm] Distancia de recubrimiento
-dp = 5
-# [cm] Altura sección
-h = 60
-# [cm] Altura efectiva de la seccion
-d = h-dp
-# [cm] Ancho
-b = 40
-# [kgf/cm2] Resistencia a compresión del hormigon
-Es = 2100000
-# Deformación del 3 por mil
-eu = 0.003
-# Valor de beta basado en resistencia del hormigon
-
+""" Valor de beta """
 
 def beta(fc):
     if fc < 280:
@@ -24,112 +9,122 @@ def beta(fc):
         beta1 = 0.65
     return beta1
 
+""" Area barras de acero """
 
-fc = 250
-# [kgf/cm2] Modulo de elasticidad del acero
-beta1 = beta(fc)
-# [kgf/cm2] Resistencia del acero A63-42H
-fy = 4200
-# [cm2] Area de acero para el refuerzo superior
-As_sup = 19.6
-# [cm2] Area de cada nivel de refuerzo lateral
-A_lat = 9.8
-# Numero de refuerzos laterales
-n_lat = 2
-# [cm2] Area de acero para el refuerzo inferior
-As_inf = 19.6
-# [cm2] Lista de areas de acero por nivel
-A = np.append([As_sup], np.append(np.array([A_lat for i in range(n_lat)]), [As_inf]), axis=0)
-# [cm] valor de c inicial correspondiente a 3/8 del valor de d
+def Acirc(D):
 
-""" Calculo de flexion simple"""
-# Valor de c para condicion balanceada
-cb = 3*d/8
-# [kgf/cm2] Lista de resistencias del acero por nivel rellena por ceros
-Fsi = np.zeros(len(A))
-# Lista de posiciones de cada nivel de acero
-y = np.array([5+i*round((h-2*dp)/(len(A)-1), 3) for i in range(len(A))])
-# Lista de deformaciones por nivel de acero
-ei = np.array([round(eu*(cb-y[i])/cb, 6) for i in range(len(A))])
-# Deformacion del acero necesaria para que entre en fluencia
-ey = fy/Es
-# Valor inicial de PS
-Ps = 0
-# Valor inicial de error
-error = 1
-# Actualizacion de ei para las iteraciones de Fsi y c mientras no exista un error menor a 0.00001
-while abs(error) > 0.00001:
-    ei = np.array([round(eu*(c-y[i])/c, 6) for i in range(len(y))])
-    # [kgf/cm2] actualizacion de Fsi por cada cambio de c
+    Ac = round(D**2*0.7854, 3)
+    return Ac
+
+""" Lista de Areas de acero por nivel """
+
+def Alist(Dsup, Dlat, nlat, Dinf):
+
+    Asup = Acirc(Dsup)
+    Alat = Acirc(Dlat)
+    Ainf = Acirc(Dinf)
+    A = [Asup]
+    for i in range(nlat):
+        A.append(Alat)
+    A.append(Ainf)
+    return A
+
+""" Lista de posicion de niveles de areas """
+
+def Ylist(h, dp, nlat):
+
+    Y = [dp]
+    for i in range(1,nlat+1):
+        Y.append((h-Y[i-1]-dp)/(nlat+2-i)+Y[i-1])
+        if Y[i]-int(Y[i])>0.5:
+            Y[i]=int(Y[i])+1
+        else:
+            Y[i] = int(Y[i])
+    Y.append(h-dp)
+    return Y
+
+""" Lista de valores de ei """
+
+def eiList(Y, c):
+
+    ei = []
+    for i in range(len(Y)):
+        ei.append((c-Y[i])/c * 0.003)
+    return ei
+
+""" Lista de valores de fs en funcion de ei """
+
+def fsList(ei, ey, fy, Es):
+
     for i in range(len(ei)):
         if ei[i] > ey:
-            Fsi[i] = fy
+            fs = fy
         elif -ey <= ei[i] <= ey:
-            Fsi[i] = Es*ei[i]
+            fs = Es * ei[i]
         elif ei[i] < -ey:
-            Fsi[i] = -fy
-    # [kgf/cm2] Lista de valores para la resistencia axial del acero
-    for i in range(len(Fsi)):
-        Ps = Fsi[i]*A[i]
-    # [cm] Valor de c anterior
-    c2 = -Ps/(0.85*beta1*fc*b)
-    # [cm] Nuevo valor de c, correspondiente a la semi-suma de su valor anterior y su valor actual
-    c = round((c2+c)/2, 4)
-    # valor del error basado en diferencias de valor anterior y nuevo
-    error = (c2-c)/c
-# [kgf] Resistencia a compresion generada por el hormigon
-Pc = 0.85*beta1*fc*b*c
-# [kgf * cm] Momento de compresion generado por la zona de compresion
-Mc = Pc*(h/2-0.85*c/2)
-# Valor inicial de Ms
-Ms = 0
-# Valor inicial de As
-As = 0
-for i in range(len(y)):
-    # [kgf * cm] Sumatoria de momentos por nivel de acero
-    Ms = Ms+Fsi[i]*A[i]*(h/2-y[i])
-    # [cm2] Sumatoria de areas por nivel de acero
-    As = As + A[i]
-# [tf * m] Valor de momento nominal con cambio de unidades
-Mn = (Ms + Mc)/100000.
-print(Ms/100000)
-print(Mc/100000)
-print(Mn)
+            fs = -fy
+        return fs
 
-""" Calculo de compresion pura """
+""" Lista de Ps """
 
-# [kgf] Resistencia nominal a compresion
-Pnc = 0.85*fc*h*b + As*fy
-# [kgf] Resistencia nominal a compresion con factor de reduccion Ø
-phiPnc = 0.65*Pnc
-# [kgf] Resistencia nominal a compresion sin area de acero
-Pnc80 = 0.8*phiPnc
+def psList(fs, A):
 
-""" Calculo de traccion pura """
+    ps = []
+    for i in range(len(A)):
+        ps = fs[i]*A[i]
+    return ps
 
-# [kgf] Resistencia nominal a traccion
-Pnt = -As*fy
-# Factor de reduccion en traccion
-phi = 0.9
-# [kgf] Resistencia nominal a traccion con factor de reduccion Ø
-phiPnt = phi*Pnt
+""" Calculo Pc """
 
-""" Calculo de condicion balanceada """
+def Pc(beta, fc, b, c):
 
-Nei = np.zeros(len(y))
-Fsib = np.zeros(len(y))
-Psib = np.zeros(len(y))
-for i in range(len(y)):
-    Nei[i] = 0.003*(c-y[i])/c
-for i in range(len(Nei)):
-    if Nei[i] > ey:
-        Fsib[i] = fy
-    elif -ey <= ei[i] <= ey:
-        Fsib[i] = Es * ei[i]
-    elif ei[i] < -ey:
-        Fsib[i] = -fy
-        
-""" ɛ = 0.005 """
-""" Tabla resumen """
-""" Curvas de interaccion """
-""" Calculo de FU """
+    pc = 0.85*beta*fc*b*c
+    return pc
+
+"""  Calculo de ØPn """
+
+def phiPn(phi, ps, pc):
+    pn = phi*(ps + pc)
+    return pn
+
+""" Calculo Ms  """
+
+def Ms(ps, h, Y):
+
+    for i in range(len(ps)):
+        ms = ps[i]*(0.5*h-Y[i])
+    return ms
+
+""" Calculo Mc """
+
+def Mc(pc, h, c):
+
+    mc = 0.5*pc*(h-0.85*c)
+    return mc
+
+""" Calculo de ØMn """
+
+def phiMn(phi, ms, mc):
+
+    mn = phi*(ms+mc)
+    return mn
+
+""" Calcular valor de c en linea neutra """
+
+def cLN(h, b, dp, pc, fc, A, Y, ey, fy, Es):
+    
+    c1 = dp
+    c2 = h-dp
+    while abs(pn)>0.00001:
+        c = (c1 + c2) / 2
+        pc = Pc(beta, fc, b, c)
+        ps = psList(fs, A)
+        ei = eiList(Y, c)
+        fs = fsList(ei, ey, fy, Es)
+        pn = pc + ps
+        if pn<0:
+            c1 = c
+        else:
+            c2 = c
+    return c
+
