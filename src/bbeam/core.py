25d7196dc26ed200d14faadc15a6b08ae6723dcc
patod01 <patocv2@hotmail.com>
2021-04-26 10:41:37 -0400

    input reader + opticore
    - nuevas fotos
    - ahora lee input (al fin!)
    - generacion de arbol de trabajo (creacion, renovacion, reutilizacion)
    - core pseudo glue function
    - verbosity moved
    - papiro added

---
+++ src/bbeam/core.py
@@ -0,0 +1,46 @@
+costos = []
+costo
+costo_min
+drift
+drift_max = 2/1.2
+perfiles = []
+perfil_a
+perfil_b
+DESIGN = False
+KAPUT = 0
+
+def verDrift(): pass
+def optijandro(perfil=[], design=True): pass
+def update(): pass
+
+def optimize():
+    while not DESIGN:
+        drift, perfil_b = verDrift()
+        if perfil_a != perfil_b:
+            perfil_a = perfil_b
+            update()
+        else:
+            DESIGN = True
+    while DESIGN:
+        costo, perfil_b = optijandro(perfil_a)
+        if perfil_a != perfil_b:
+            perfil_a = perfil_b
+            update()
+        i = 0
+        while True:
+            i += 1
+            drift, perfil_b = verDrift()
+            if drift <= drift_max and i = 1:
+                perfiles.append(perfil_a)
+                costos.append(costo)
+            if perfil_a != perfil_b:
+                perfil_a = perfil_b
+                update()
+            else:
+                 break
+        if len(costos) == verificaciones or KAPUT == 100:
+            c = costos.index(min(costos))
+            perfil_a = perfiles[c]
+            DESIGN = False
+            detalle = optijandro(perfil_a, DESIGN)
+    return
